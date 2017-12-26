2250a07bfa24a67f4eac73cb907b9058a89c582e
tmosharaf7 <mosharaftanvir7@gmail.com>
2017-12-26 20:34:07 +0600

    almost done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var filterizd = $('.filtr-container').filterizr({
+   //options object
+});
+
+
+
+
+
 var selectIds = $('#panel1,#panel2,#panel3,#panel4');
 $(function ($) {
     selectIds.on('show.bs.collapse hidden.bs.collapse', function () {
@@ -48,3 +56,66 @@ $(function ($) {
                     $('.searchbox-icon').css('display','block');
                 }
             }
+
+
+
+(function($) {
+  $('.skill_circle_1').circleProgress({
+    value: 0.85,
+      thickness: 5,
+      emptyFill: "#555555",
+      animation: { duration: 1700, easing: "circleProgressEasing" },
+    fill: {gradient: ['#E86153', '#FECB16']}
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(6587 * progress) + '<i></i>');
+  });
+
+  $('.skill_circle_2').circleProgress({
+    value: 0.5,
+      thickness: 5,
+      emptyFill: "#555555",
+      animation: { duration: 1700, easing: "circleProgressEasing" },
+    fill: {gradient: ['#E86153', '#FECB16']}
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(6587 * progress) + '<i></i>');
+  });
+
+  $('.skill_circle_3').circleProgress({
+    value: 0.7,
+      thickness: 5,
+      emptyFill: "#555555",
+      animation: { duration: 1700, easing: "circleProgressEasing" },
+    fill: {gradient: ['#E86153', '#FECB16']}
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(6587 * progress) + '<i></i>');
+  });
+  $('.skill_circle_4').circleProgress({
+    value: 0.4,
+      thickness: 5,
+      emptyFill: "#555555",
+      animation: { duration: 1700, easing: "circleProgressEasing" },
+    fill: {gradient: ['#E86153', '#FECB16']}
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('strong').html(Math.round(6587 * progress) + '<i></i>');
+  });
+
+
+})(jQuery);
+
+$(document).ready(function(){
+  $(".slide_all").owlCarousel({
+      items: 1,
+      loop: true,
+      nav: true,
+  navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],
+      dots: 0
+  });
+  $(".testimonials_all").owlCarousel({
+      items: 1,
+      loop: 0,
+      dots: true,
+  });
+    
+    
+    
+});
